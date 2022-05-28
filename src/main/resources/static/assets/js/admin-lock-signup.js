0a96ed1989b92d2616b78665f59074238d5f1ef0
Bimsara Bodaragama <bodaragamadb.office@gmail.com>
2022-05-29 04:09:12 +0530

    Fix bug in login in

--- src/main/resources/static/assets/js/admin-lock-signup.js
+++
@@ -1,5 +0,0 @@
-$( "#adminLockSignUp" ).click(function() {
-//adminLockSignUp
-  console.log( "admin-lock-signup-get Handler for .click() called.");
-  window.location.href = "/admin-lock-signup-get";
-});
